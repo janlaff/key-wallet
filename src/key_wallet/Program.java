50ed8561cd6450cf1a9a0e1742c4856a6146e48f
Jan Lafferton <jan@lafferton.de>
2020-10-19 17:22:52 +0200

    Add master password prompt

--- src/key_wallet/Program.java
+++ src/key_wallet/Program.java
@@ -1,7 +1,9 @@
 package key_wallet;
 
+import key_wallet.controllers.MasterPasswordController;
+
 public class Program {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println(MasterPasswordController.getInstance().getMasterPassword());
     }
 }
--- src/key_wallet/controllers/MasterPasswordController.java
+++ src/key_wallet/controllers/MasterPasswordController.java
@@ -1,5 +1,7 @@
 package key_wallet.controllers;
 
+import javax.swing.*;
+
 public class MasterPasswordController {
     private static MasterPasswordController instance;
     private MasterPasswordController() {}
@@ -11,8 +13,48 @@ public class MasterPasswordController {
         return instance;
     }
 
+    private String getMasterPasswordFromUser() {
+        JPanel panel = new JPanel();
+        JLabel label = new JLabel("Enter the master password to unlock");
+        JPasswordField password = new JPasswordField(10);
+
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+        panel.add(label);
+        panel.add(password);
+
+        String[] options = new String[] { "Ok", "Cancel" };
+        int option = JOptionPane.showOptionDialog(
+                null,
+                panel,
+                "Master Password Prompt",
+                JOptionPane.NO_OPTION,
+                JOptionPane.PLAIN_MESSAGE,
+                null,
+                options,
+                options[0]
+        );
+
+        if (option == 0) {
+            char[] masterPassword = password.getPassword();
+            return new String(masterPassword);
+        } else {
+            throw new RuntimeException("User aborted password prompt");
+        }
+    }
+
+    private boolean validateMasterPassword(String masterPassword) {
+        // TODO: validate somehow (eg Stored hash)
+        return true;
+    }
+
     public String getMasterPassword() {
         // TODO: validate access and prompt for password if necessary
-        return "MasterPassword";
+        String userInput = getMasterPasswordFromUser();
+
+        if (validateMasterPassword(userInput)) {
+            return userInput;
+        } else {
+            throw new RuntimeException("Invalid master password entered");
+        }
     }
 }
