c9a798bacc8653894a8b4d7d25da4e22b59f511f
Jan Lafferton <jan@lafferton.de>
2020-10-19 16:41:39 +0200

    Add credential controller

---
+++ src/key_wallet/controllers/CredentialController.java
@@ -0,0 +1,12 @@
+package key_wallet.controllers;
+
+public class CredentialController {
+    public int createCredential() {
+        // TODO: create credential and return id
+        return 0;
+    }
+
+    public void removeCredential(int credentialId) {
+        // TODO: remove credential
+    }
+}
