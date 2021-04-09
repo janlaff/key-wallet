741af402fe0c7a9320b19642e7ea90b54e7159c8
Adrian <schmidt.adrian@student.dhbw-karlsruhe.de>
2021-04-09 22:41:08 +0200

    Added GUI classes and coded Login and Main

--- src/key_wallet/ui/GUI.java
+++ src/key_wallet/ui/GUI.java
@@ -4,6 +4,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.beans.PropertyChangeListener;
 
 public class GUI implements ActionListener {
 
@@ -14,9 +15,44 @@ public class GUI implements ActionListener {
         log_in_button.setBounds(10,10,10,10);
         JLabel EnterP = new JLabel("Enter Master Password:");
         JPasswordField Password = new JPasswordField("Password");
-        JLabel login_message = new JLabel("");
+        JLabel login_message = new JLabel("Welcome to key-wallet!");
         login_message.setBounds(100,100,100,100);
-        log_in_button.addActionListener(new GUI());
+        log_in_button.addActionListener(new Action() {
+            @Override
+            public Object getValue(String key) {
+                return null;
+            }
+
+            @Override
+            public void putValue(String key, Object value) {
+
+            }
+
+            @Override
+            public void setEnabled(boolean b) {
+
+            }
+
+            @Override
+            public boolean isEnabled() {
+                return false;
+            }
+
+            @Override
+            public void addPropertyChangeListener(PropertyChangeListener listener) {
+
+            }
+
+            @Override
+            public void removePropertyChangeListener(PropertyChangeListener listener) {
+
+            }
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("test");
+            }
+        });
 
         JPanel panel = new JPanel();
         panel.setBorder(BorderFactory.createEmptyBorder(100,100, 100, 100));
@@ -35,7 +71,7 @@ public class GUI implements ActionListener {
     }
 
     public static void main(String[] args) {
-        new GUI();
+        new GUILogin();
 
     }
 
--- src/key_wallet/ui/GUIMain.java
+++ src/key_wallet/ui/GUIMain.java
@@ -1,2 +1,102 @@
-package key_wallet.ui;public class GUIMain {
+package key_wallet.ui;
+import javax.sound.sampled.Line;
+import javax.swing.*;
+import javax.swing.border.Border;
+import javax.swing.border.LineBorder;
+import javax.swing.SwingUtilities;
+import java.awt.Color;
+import java.awt.Dimension;
+
+
+public class GUIMain
+{
+    public static void main(String[] args){
+
+        //Construction of string and close on exit setting
+        JFrame mainframe = new JFrame("MainFrame");
+        mainframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        mainframe.setTitle("key-wallet");
+
+        //border Settings
+        Border border = new LineBorder(Color.black);
+
+        //menubar
+        JMenuBar menubar = new JMenuBar();
+        menubar.setBorder(border);
+        //menu content
+        JMenu menunew = new JMenu("New");
+        menubar.add(menunew);
+        //add menu item to menu content
+        JMenuItem test = new JMenuItem("test");
+        menunew.add(test);
+        //seperates different items in menu
+        JSeparator sep = new JSeparator();
+        menunew.add(sep);
+        JMenuItem test2 = new JMenuItem("test2");
+        menunew.add(test2);
+
+        JMenu menuedit = new JMenu("Edit");
+        menubar.add(menuedit);
+        JMenu menuview = new JMenu("View");
+        menubar.add(menuview);
+        JMenu menuhelp = new JMenu("Help");
+        menubar.add(menuhelp);
+        JMenu menuclose = new JMenu("Close");
+        menubar.add(menuclose);
+        mainframe.setJMenuBar(menubar);
+
+        //popupmenu = right mouse click menu
+        /// TODO: 09.04.2021 Add event Listener for Right Click
+        JPopupMenu rmenu = new JPopupMenu();
+        rmenu.setLocation(200,200);
+        rmenu.add("Hallo");
+        rmenu.add("hier");
+        rmenu.add("steht");
+        rmenu.add("ein");
+        rmenu.add("text");
+
+        //test panel
+        JPanel  pwlist = new JPanel();
+        pwlist.setBackground(Color.green);
+        ///TODO ADD PW list
+        pwlist.add(new JLabel("Dies ist ein Test."));
+
+        JScrollPane scrollPane;
+        scrollPane = new JScrollPane (pwlist);
+        scrollPane.setVerticalScrollBarPolicy(
+                ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+        scrollPane.setHorizontalScrollBarPolicy(
+                ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+        scrollPane.setPreferredSize(new Dimension(2000, 600));
+        Dimension minimumSize = new Dimension(600, 600);
+        pwlist.setMinimumSize(minimumSize);
+        mainframe.add(scrollPane);
+
+        JPanel  pwcontent = new JPanel();
+        pwcontent.setBackground(Color.red);
+
+        //Splitpane
+        JSplitPane splitPane = new JSplitPane();
+        splitPane.setLeftComponent(pwlist);
+        splitPane.setRightComponent(pwcontent);
+        splitPane.setDividerLocation(250);
+        splitPane.setOneTouchExpandable(true);
+        mainframe.add(splitPane);
+
+        //Set width and hight
+        mainframe.setSize(800,600);
+
+        //show contents
+        mainframe.setVisible(true);
+        rmenu.setVisible(true);
+
+        //Labels
+        JLabel text = new JLabel ("Test");
+
+        //frameContent
+        mainframe.add(text);
+
+        //
+
+    }
 }
